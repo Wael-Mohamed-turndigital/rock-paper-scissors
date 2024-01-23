3ddf762775dd41b96fe20f72faac9fecf9e7389f
Wael Mohamed <wmohamed@turndigital.net>
2024-01-23 14:18:38 +0200

    setup

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <div class="hello">hello</div>
+  </main>
+</template>
+
+<style lang="scss">
+$bgColor: #1b2a4b;
+.hello {
+  background-color: $bgColor;
+}
+
+</style>
